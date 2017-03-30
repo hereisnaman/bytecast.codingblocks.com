46984afe3656dde7df322a87c3220f2a58d55b42
Arnav Gupta <arnav@codingblocks.com>
2017-03-30 18:24:27 +0530

    add video player
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- core/functions.php
+++ core/functions.php
@@ -1072,7 +1072,16 @@ function showStreamingPlayers($filenameWithoutExtension,$podcast_filetype,$url,$
 			</audio>';
 			$playersOutput .= ''.$showplayercode.'<br />'; 
 
-		} 
+		}
+
+		if ($browserAudioVideoSupport[0] == TRUE AND $podcast_filetype=="mp4") { //if browser supports HTML5
+			$showplayercode =	'<video style="width:80%;" controls preload="metadata">
+				  <source src="'.$url.$upload_dir.$filenameWithoutExtension.'.mp4" type="video/mp4">
+				'._("Your browser does not support the video player").'
+				</video>';
+			$playersOutput .= ''.$showplayercode.'<br />';
+
+		}
 		// Flash Player disabled from version 2.5
 		/* 
 		else { //if no support for HTML5, then flash player just for mp3
