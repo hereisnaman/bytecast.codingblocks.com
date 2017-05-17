0e0d4998cd7ed9f26b8efc329900ce5de9ad6acb
hereisnaman <naman@outlook.in>
2017-05-17 22:17:25 +0530

    Changed permalink

--- core/templates.php
+++ core/templates.php
@@ -4,7 +4,7 @@
 #
 # Created by Alberto Betella
 # http://podcastgen.sourceforge.net
-# 
+#
 # This is Free Software released under the GNU/GPL License.
 ############################################################
 
@@ -21,12 +21,12 @@
 active (menu active)
 nav-header (titles in sidebar)
 
-navbar-link (links in the navbar (e.g. log out) 
+navbar-link (links in the navbar (e.g. log out)
 
 */
 
 ########### Security code, avoids cross-site scripting (Register Globals ON)
-if (isset($_REQUEST['GLOBALS']) OR isset($_REQUEST['absoluteurl']) OR isset($_REQUEST['amilogged']) OR isset($_REQUEST['theme_path'])) { exit; } 
+if (isset($_REQUEST['GLOBALS']) OR isset($_REQUEST['absoluteurl']) OR isset($_REQUEST['amilogged']) OR isset($_REQUEST['theme_path'])) { exit; }
 ########### End
 
 
@@ -58,7 +58,7 @@ $theme_file_contents = str_replace("<embed src=\"", "<embed src=\"".$theme_path,
 ####### INCLUDE PHP FUNCTIONS SPECIFIED IN THE THEME (functions.php)
 if (file_exists($theme_path."functions.php")) {
 	include ($theme_path."functions.php");
-}	
+}
 ####### END INCLUDE PHP FUNCTIONS
 
 
@@ -73,32 +73,32 @@ if (isset($_GET['cat']) AND $_GET['cat'] != "all" AND !isset($_GET['action'])) {
 	if (isset($existingCategories[avoidXSS($_GET['cat'])])) {
 		//URL depuration (avoidXSS)
 		$page_title_prefix .= $existingCategories[avoidXSS($_GET['cat'])]." - ";
-		}	
+		}
 	}
 	//Show a generic "All episodes"
 	elseif (isset($_GET['p']) AND $_GET['p']=="archive") {
 		$page_title_prefix .= _("All Episodes")." - ";
 	}
-	
-	
+
+
 	//if is single episode, add title of episode to title of page
 	elseif (isset($_GET['name'])) {
-	
+
 		$titleOfEpisode = showSingleEpisode(avoidXSS($_GET['name']),1); //the last parameter (1) requires just the title to that function
-		
+
 		if ($titleOfEpisode != NULL) $page_title_prefix .= "$titleOfEpisode - ";
-	
+
 	}
-	
+
 
 $page_title = $page_title_prefix.$podcast_title;
-	
-$theme_file_contents = str_replace("-----PG_PAGETITLE-----", $page_title, $theme_file_contents);  
+
+$theme_file_contents = str_replace("-----PG_PAGETITLE-----", $page_title, $theme_file_contents);
 
 
 include($absoluteurl."core/admin/loadjavascripts.php");
 
-$theme_file_contents = str_replace("-----PG_JSLOAD-----", $loadjavascripts, $theme_file_contents); 
+$theme_file_contents = str_replace("-----PG_JSLOAD-----", $loadjavascripts, $theme_file_contents);
 
 ###############################
 ###############################
@@ -109,7 +109,7 @@ $theme_file_contents = str_replace("-----PG_JSLOAD-----", $loadjavascripts, $the
 $commonCSSurl = '<link href="themes/common.css" rel="stylesheet">';
 //ADDING FONT AWESOME, FOR AWESOME ICONS
 $commonCSSurl .= '<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">';
-$theme_file_contents = str_replace("-----PG_COMMONCSSLOAD-----", $commonCSSurl, $theme_file_contents); 
+$theme_file_contents = str_replace("-----PG_COMMONCSSLOAD-----", $commonCSSurl, $theme_file_contents);
 
 
 
@@ -118,7 +118,7 @@ $theme_file_contents = str_replace("-----PG_COMMONCSSLOAD-----", $commonCSSurl, 
 // SET PODCAST FEED custom or default URL
 if (isset($feed_URL_replace) AND $feed_URL_replace != "") {
 $podcastFeedURL = $feed_URL_replace;
-$podcastFeedURLiTunes = str_replace("http://", "itpc://", $podcastFeedURL); 
+$podcastFeedURLiTunes = str_replace("http://", "itpc://", $podcastFeedURL);
 } else {
 $podcastFeedURL = $url.$feed_dir.'feed.xml';
 $podcastFeedURLiTunes = str_replace("http://", "itpc://", $podcastFeedURL);
@@ -146,10 +146,10 @@ if(isUserLogged()) { //if admin page
 
 	//show donation box after 3 days from installation
 	if (isset($first_installation) and time()-$first_installation>259200) {//259200 seconds = 3 days
-	
+
 		if (isset($author_name) and $author_name != NULL) $nameToAddressUser = $author_name.", ";
 		else $nameToAddressUser = NULL;
-		
+
 		$adminrightboxcontent .= '
 			<div class="rightbox">
 			<span class="nav-header">'._("Support Podcast Generator").'</span>
@@ -161,8 +161,8 @@ if(isUserLogged()) { //if admin page
 			</div>
 		';
 	}
-	
-	
+
+
 	//show PG box
 	$adminrightboxcontent .= '
 		<div class="rightbox">
@@ -175,12 +175,12 @@ if(isUserLogged()) { //if admin page
 		</ul>
 		</div>
 	';
-	
+
 }
 
 $theme_file_contents = str_replace("-----PG_RIGHTBOX-----", $rightboxcontent, $theme_file_contents);
 
-$theme_file_contents = str_replace("-----PG2_ADMINRIGHTBOX-----", $adminrightboxcontent, $theme_file_contents); 
+$theme_file_contents = str_replace("-----PG2_ADMINRIGHTBOX-----", $adminrightboxcontent, $theme_file_contents);
 
 
 # SET RIGHT OPTIONAL BOX ("freebox")
@@ -191,44 +191,44 @@ $freeboxcontent = NULL;
 		if(file_exists($absoluteurl."freebox-content.txt")){
 
 			$freeboxcontenttodisplay = file_get_contents($absoluteurl."freebox-content.txt");
-						
+
 			$freeboxcontent = '<div class="rightbox">
 				'.$freeboxcontenttodisplay.'
 				</div>';
 		}
 
-		$theme_file_contents = str_replace("-----PG_FREEBOX-----", $freeboxcontent, $theme_file_contents); 
+		$theme_file_contents = str_replace("-----PG_FREEBOX-----", $freeboxcontent, $theme_file_contents);
 
 	} else {
 
 		$freeboxcontent = NULL;
 
-		$theme_file_contents = str_replace("-----PG_FREEBOX-----", $freeboxcontent, $theme_file_contents); 		
+		$theme_file_contents = str_replace("-----PG_FREEBOX-----", $freeboxcontent, $theme_file_contents);
 
 	}
 
-	
-	
-	
+
+
+
 
 	# Othere Theme elements replacing
 	$theme_file_contents = str_replace("-----PG_MAINBODY-----", $PG_mainbody, $theme_file_contents);
 
-	$theme_file_contents = str_replace("-----PG_PAGECHARSET-----", $feed_encoding, $theme_file_contents); 
+	$theme_file_contents = str_replace("-----PG_PAGECHARSET-----", $feed_encoding, $theme_file_contents);
 
 	$theme_file_contents = str_replace("-----PG_PODCASTTITLE-----", $podcast_title, $theme_file_contents);
 
 	$theme_file_contents = str_replace("-----PG_PODCASTSUBTITLE-----", $podcast_subtitle, $theme_file_contents);
 
-	$theme_file_contents = str_replace("-----PG_PODCASTDESC-----", $podcast_description, $theme_file_contents); 
-	
-	
-	$theme_file_contents = str_replace("-----PG2_URLRSSFEED-----", $podcastFeedURL, $theme_file_contents); 
-	
-	$theme_file_contents = str_replace("-----PG2_URLFORITUNES-----", $podcastFeedURLiTunes, $theme_file_contents); 
+	$theme_file_contents = str_replace("-----PG_PODCASTDESC-----", $podcast_description, $theme_file_contents);
 
-	
-	
+
+	$theme_file_contents = str_replace("-----PG2_URLRSSFEED-----", $podcastFeedURL, $theme_file_contents);
+
+	$theme_file_contents = str_replace("-----PG2_URLFORITUNES-----", $podcastFeedURLiTunes, $theme_file_contents);
+
+
+
 #### MENU TOP
 // Replace menu top (class active assigned to the active menu)
 
@@ -254,14 +254,14 @@ if ($categoriesenabled == "yes") { //if categories are enabled
 		<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">'._("Archive").' <b class="caret"></b></a>
 					<ul class="dropdown-menu">';
-			
+
 
 			// READ THE CATEGORIES
 	$existingCategories = readPodcastCategories ($absoluteurl);
 	//var_dump($existingCategories); //Debug
-			
+
 		ksort($existingCategories);	//sort array by key alphabetically
-		
+
 		for ($i = 0; $i <  count($existingCategories); $i++) {
 		$key=key($existingCategories);
 		$val=$existingCategories[$key];
@@ -271,7 +271,7 @@ if ($categoriesenabled == "yes") { //if categories are enabled
 		 next($existingCategories);
 		}
 		// END - READ THE CATEGORIES
-			
+
 	 $contentmenuarchive .= '
 	 <li class="divider"></li>
 	 <li><a href="?p=archive&amp;cat=all">'._("All Episodes").'</a></li>
@@ -283,8 +283,8 @@ $contentmenuarchive = '<li';
 if (isset($_GET['p']) and $_GET['p'] == "archive") $contentmenuarchive .= ' class="active"';
 $contentmenuarchive .= '><a href="?p=archive">'._("All Episodes").'</a></li>';
 }
- 
- 
+
+
 
 $theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", $contentmenuarchive, $theme_file_contents);
 
@@ -292,11 +292,11 @@ $theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", $contentmenuarchi
 
 
 
-//	$theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", _("Podcast Archive"), $theme_file_contents); 
-	
-	
+//	$theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", _("Podcast Archive"), $theme_file_contents);
 
-	$theme_file_contents = str_replace("-----PG_MENUADMIN-----", $loginmenu, $theme_file_contents); 
+
+
+	$theme_file_contents = str_replace("-----PG_MENUADMIN-----", $loginmenu, $theme_file_contents);
 
 	#FOOTER
 
@@ -325,14 +325,14 @@ $theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", $contentmenuarchi
 	# define META KEYWORDS
 
 	// on single episode page (permalink), use itunes keywords and episode description as meta tags...
-	if (isset($_GET['p']) AND $_GET['p']=="episode" AND isset($episode_present) AND $episode_present == "yes") { 
+	if (isset($_GET['p']) AND $_GET['p']=="episode" AND isset($episode_present) AND $episode_present == "yes") {
 		if ($text_keywordspg != NULL) { // ...if keywords exist
 			$metatagstoreplace .= '<meta name="Keywords" content="'.depuratecontent($text_keywordspg).'" />
 				';
 		}
 		$metatagstoreplace .= '<meta name="Description" content="'.depuratecontent($text_shortdesc).'" />
 			'; // use episode short description
-	} 
+	}
 	else { // if not permalink page, use podcast general description as meta tag
 		$metatagstoreplace .= '<meta name="Description" content="'.depuratecontent($podcast_description).'" />
 			';
@@ -342,75 +342,75 @@ $theme_file_contents = str_replace("-----PG_MENUARCHIVE-----", $contentmenuarchi
 	// on the home page (recent_list.php) use keywords of the most recent episode
 	if (isset($assignmetakeywords) AND $assignmetakeywords != NULL) { // the variable $assignmetakeywords is assigned in recent_list.php
 		$metatagstoreplace .= '<meta name="Keywords" content="'.depuratecontent($assignmetakeywords).'" />
-			';	
+			';
 	}
 
 
 	// general XML feed of the podcast
 	$metatagstoreplace .= '
 		<link href="'.$podcastFeedURL.'" rel="alternate" type="application/rss+xml" title="'.$podcast_title.' RSS" />';
-		
-		
-		
+
+
+
 	//CUSTOMIZE THE PAGES DEDICATED TO SINGLE EPISODES (with dedicated meta tags to increase SEO)
-	
+
 	//reconstruct the full URL of the episode
-			
+
 			if (isset($_GET['name'])) {
-			
-			$episodeURLreconstructed = $url.'?name='.avoidXSS($_GET['name']);
-			
+
+			$episodeURLreconstructed = $url.''.avoidXSS($_GET['name']);
+
 			// then ADD SOME OPEN GRAPH META TAGS
 			$metatagstoreplace .= '
 			<meta property="og:title" content="'.$titleOfEpisode.' &laquo; '.$podcast_title.'"/>
 			<meta property="og:url" content="'.$episodeURLreconstructed.'"/>
 			';
-	
+
 			// and the canonical link
 			$metatagstoreplace .= '
 			<link rel="canonical" href="'.$episodeURLreconstructed.'" />
 			';
-			} 
-			
+			}
+
 			else { //IF IS HOME PAGE
-			
+
 			$metatagstoreplace .= '
 			<meta property="og:title" content="'.$podcast_title.'"/>
 			<meta property="og:url" content="'.$url.'"/>
 			<meta property="og:image" content="'.$url.$img_dir.'bytecast_image.png"/>
 			';
-	
+
 			// and the canonical link
 			$metatagstoreplace .= '
 			<link rel="canonical" href="'.$url.'" />
 			';
-			} 
-			
+			}
+
 
 	$theme_file_contents = str_replace("-----PG_METATAGS-----", $metatagstoreplace, $theme_file_contents);
 
 	# END META TAGS DEFINITION
 	#########################
 
-	
-	
-	
-	
-//INCLUDE LOADING INDICATOR IN ALL THE PAGES 
+
+
+
+
+//INCLUDE LOADING INDICATOR IN ALL THE PAGES
 //The indicator is included in all the pages just before closing the tag </html> so we are sure it is outside others relative containers (e.g. bootstrap theme). Otherwise it would be displayed relative to the main container, not to the body
 
 if (isset($_GET['p']) AND $_GET['p']=="admin") { //all admin pages included login
-		
+
 //NB the closing body tag has been added below!
 $loading_indicator_code = '
 <div id="status_notification">Uploading...</div>
 </body>
 ';
-			
+
 	$theme_file_contents = str_replace("</body>", $loading_indicator_code, $theme_file_contents);
-	
+
 }
-	
-//END - INCLUDE LOADING INDICATOR IN ALL THE PAGES 
+
+//END - INCLUDE LOADING INDICATOR IN ALL THE PAGES
 
 	?>
